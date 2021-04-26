62700cd9a6d04deb5378070479d00734afdf3149
Thomas Boilesen <thomasboilesen@gmail.com>
2021-04-25 21:54:50 -0300

    mudei fonte de dados para o gecko

--- src/App.js
+++ src/App.js
@@ -1,123 +1,8 @@
-// import React, { useState, useEffect } from "react";
-// import "semantic-ui-css/semantic.min.css";
-// import { Card, Dimmer, Loader, Select } from "semantic-ui-react";
-// import ApexCharts from "apexcharts";
-
-// import Chart from "react-apexcharts";
-// import "./App.css";
-
-// const options = [
-//   { value: "USD", text: "USD" },
-//   { value: "EUR", text: "EUR" },
-//   { value: "GBP", text: "GBP" },
-//   { value: "BRL", text: "BRL" },
-// ];
-
-// function App() {
-//   const [loading, setLoading] = useState(true);
-//   const [priceData, setPriceData] = useState(null);
-//   const [currency, setCurrency] = useState("USD");
-//   const [chartData, setChartData] = useState(null);
-//   const [series, setSeries] = useState(null);
-
-//   useEffect(() => {
-//     async function fetchData() {
-//       const res = await fetch(
-//         "https://api.coindesk.com/v1/bpi/currentprice.json"
-//       );
-//       const data = await res.json();
-//       setPriceData(data.bpi);
-//       getChartData();
-//     }
-//     fetchData();
-//   }, []);
-
-//   const handleSelect = (e, data) => {
-//     setCurrency(data.value);
-//   };
-
-//   const getChartData = async () => {
-//     const res = await fetch(
-//       "https://api.coindesk.com/v1/bpi/currentprice.json"
-//     );
-//     const data = await res.json();
-//     const categories = Object.keys(data.bpi);
-//     const series = Object.values(data.bpi);
-//     setChartData({
-//       xaxis: {
-//         categories: categories,
-//       },
-//     });
-
-//     setSeries([
-//       {
-//         name: "Bitcoin Price",
-//         data: series,
-//       },
-//     ]);
-
-//     setLoading(false);
-//   };
-
-//   return (
-//     <div className="App">
-//       <div
-//         className="nav"
-//         style={{ padding: "15", color: "#111111", backgroundColor: "gold" }}
-//       >
-//         Coindesk Api Data
-//       </div>
-//       {loading ? (
-//         <div>
-//           <Dimmer active inverted>
-//             <Loader>Loading</Loader>
-//           </Dimmer>
-//         </div>
-//       ) : (
-//         <div
-//           className="price-container"
-//           style={{
-//             display: "flex",
-//             justifyContent: "space-around",
-//             alignItems: "center",
-//             width: 600,
-//             height: 300,
-//             margin: "0 auto",
-//           }}
-//         >
-//           <div className="form">
-//             <Select
-//               placeholder="selecione a moeda"
-//               nChange={handleSelect}
-//               options={options}
-//             ></Select>
-//           </div>
-//           <div className="price">
-//             <Card>
-//               <Card.Content>
-//                 <Card.Header>{currency} Moeda</Card.Header>
-//                 <Card.Description>{priceData[currency].rate}</Card.Description>
-//               </Card.Content>
-//             </Card>
-//           </div>
-//           <Chart
-//             options={chartData}
-//             serues={series}
-//             type="line"
-//             width="1200"
-//             height="300"
-//           />
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
 import React, { useState, useEffect } from "react";
 import "semantic-ui-css/semantic.min.css";
 import { Card, Dimmer, Loader, Select } from "semantic-ui-react";
 import ApexCharts from "apexcharts";
+import moment from "moment";
 
 import Chart from "react-apexcharts";
 import "./App.css";
@@ -150,14 +35,22 @@ function App() {
 
   const getChartData = async () => {
     const res = await fetch(
-      `https://api.coindesk.com/v1/bpi/historical/close.json?`
+      `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1392577232&to=1422577232`
     );
     const data = await res.json();
-    const categories = Object.keys(data.bpi);
-    const series = Object.values(data.bpi);
+    const categories = Object.values(data.prices.map((X) => X[0]));
+    const series = Object.values(data.prices.map((X) => X[1]));
+    const time = categories.map(function (x) {
+      return new Date(x).toLocaleDateString("en-US");
+    });
+
+    // let formatted_date =
+    //   time.getDate() + "-" + (time.getMonth() + 1) + "-" + time.getFullYear();
+    // console.log(formatted_date);
+
     setChartData({
       xaxis: {
-        categories: categories,
+        categories: time,
       },
     });
     setSeries([
